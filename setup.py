3a0a701c06290b7741dafb0ca0a57d5bbb977c98
Alexander Grishchenko <alexanderdemure@gmail.com>
2025-04-26 12:36:56 +0500

    Initial Commit

--- setup.py
+++ setup.py
@@ -6,8 +6,11 @@ with open("requirements.txt") as f:
 
 setup(
     name="gadlint",
-    version="0.0.1",
+    version="0.0.2",
     packages=find_packages(),
+    package_data={
+        "gadlint": ["configs/*", "configs/.isort.cfg"],
+    },
     install_requires=requirements,
     entry_points={
         "console_scripts": [
